28fb3d1f0349c291f4bdafa67645478d293b9b12
Ericka Ruiz <109981852+ErickaRuiz36@users.noreply.github.com>
2025-02-20 03:00:38 -0600

    Operacion de modulo
    %

--- calculadora.py
+++ calculadora.py
@@ -4,7 +4,7 @@ def calculadora():
     
     try:
         num1 = float(input("Ingrese el primer número: "))
-        operador = input("Ingrese la operación (+, -, *, /): ")
+        operador = input("Ingrese la operación (+, -, *, /, %): ")
         num2 = float(input("Ingrese el segundo número: "))
         
         if operador == '+':
@@ -13,6 +13,8 @@ def calculadora():
             resultado = num1 - num2
         elif operador == '*':
             resultado = num1 * num2
+        elif operador == '%':
+            resultado = num1 % num2
         elif operador == '/':
             if num2 == 0:
                 print("Error: No se puede dividir por cero.")
